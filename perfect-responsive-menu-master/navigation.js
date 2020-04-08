7df8755f514bbca9093228e07e329693cd7f70ca
Mattou <mathieu.joubert@live.fr>
2020-04-08 10:00:03 +0200

    test
    modif

--- perfect-responsive-menu-master/navigation.js
+++ perfect-responsive-menu-master/navigation.js
@@ -1,8 +1,9 @@
 const toggleMenu = document.querySelector('.navigation button');
 const menu = document.querySelector('.navigation ul');
 
-toggleMenu.addEventListener('click', function() {
+toggleMenu.addEventListener('click', function () {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
 	toggleMenu.setAttribute('aria-expanded', !open);
 	menu.hidden = !menu.hidden;
+	document.body.classList.toggle('noscroll');
 });
